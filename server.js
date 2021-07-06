0122d117adddc043240c452d4a6b7a9a98df746d
cjy324 <juy32400@gmail.com>
2021-07-06 18:01:01 +0900

    21.07.06 업로드와 다운로드는 가능, 클라이언트와 서버간의 통신 문제 고민중

--- server.js
+++ server.js
@@ -11,32 +11,49 @@ const port = 8085;
 // multer 모듈 인스턴스 생성
 // Multer는 multipart/form-data 처리를 위한 미들웨어임
 const multer = require('multer');
+// fs는 node.js에 들어 있는 module로 file system의 약자입니다. 
+// 서버의 파일/폴더에 접근할 수 있는 함수들이 들어 있습니다. 
+// fs 모듈은 파일업로드와 직접적인 연관이 있는 모듈은 아니고, 업로드될 파일을 저장할 폴더를 생성하기 위해서만 사용했습니다.
+const fs = require('fs');
 
 
 // ./uploads 에 파일을 저장한다는 의미 만약에 uploads 폴더가 없을 시 자동 생성한다.
 let upload = multer({ dest: './uploads' });
 
 // 업로드된 파일의 원래 이름으로 파일명을 변경해준다.
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+const storage = multer.diskStorage({  // diskStorage: 파일을 디스크에 저장하기 위한 모든 제어 기능을 제공
+    destination: function (req, file, cb) {   // diskStorage옵션  ->  destination: 파일을 저장할 폴더 경로, 경로 설정 전 폴더가 있는지 확인
         cb(null, './uploads')
     },
-    filename: function (req, file, cb) {
+    filename: function (req, file, cb) {   // diskStorage옵션  ->  filename: 파일의 이름 지정
         cb(null, file.originalname)
     }
 })
 upload = multer({storage : storage})
 
-
 /* upload 경로 */
 app.post('/doUpload',  upload.any(), (req, res) => {
-    console.log(req.files)
+  console.log(req.files)
+
 })
 
-// fs는 node.js에 들어 있는 module로 file system의 약자입니다. 
-// 서버의 파일/폴더에 접근할 수 있는 함수들이 들어 있습니다. 
-// fs 모듈은 파일업로드와 직접적인 연관이 있는 모듈은 아니고, 업로드될 파일을 저장할 폴더를 생성하기 위해서만 사용했습니다.
-const fs = require('fs');
+
+//route to download a file
+app.get('/download/:file(*)',(req, res) => {
+  const file = req.params.file;
+  const fileLocation = path.join('./uploads', file);
+  console.log(fileLocation);
+  res.download(fileLocation, file);
+  console.log(file)
+});
+
+app.get('/download2',(req, res) => {
+  console.log('log1');
+
+  
+
+});
+
 
 
 
