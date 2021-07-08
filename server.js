807f42280943b67e56d57a7280b8b231f70dcfef
cjy324 <juy32400@gmail.com>
2021-07-08 09:52:47 +0900

    21.07.08 불필요한 주석 삭제 등 코드 정리

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-// express 참고: https://codingcoding.tistory.com/560
-// multer로 업로드하기 참고: https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=psj9102&logNo=221127724119
-
 // express 모듈 인스턴스 작성
 const express = require('express');
 const app = express();
-// router 
-const router = express.Router();
 // path 지정용 모듈
 const path = require('path');
 // port 지정
@@ -13,10 +8,6 @@ const port = 8085;
 // multer 모듈 인스턴스 생성
 // multer는 multipart/form-data 처리를 위한 미들웨어임
 const multer = require('multer');
-// fs는 node.js에 들어 있는 module로 file system의 약자입니다. 
-// 서버의 파일/폴더에 접근할 수 있는 함수들이 들어 있습니다. 
-// fs 모듈은 파일업로드와 직접적인 연관이 있는 모듈은 아니고, 업로드될 파일을 저장할 폴더를 생성하기 위해서만 사용
-// const fs = require('fs');
 
 // ./uploads 에 파일을 저장한다는 의미 만약에 uploads 폴더가 없을 시 자동 생성한다.
 let upload = multer({ dest: './uploads' });
@@ -41,6 +32,7 @@ app.post('/doUpload',  upload.any(), (req, res) => {
     fileNames[i] = req.files[i].originalname;
   }
   fileNames = JSON.stringify(fileNames)  // stringify: 객체를 json 형식으로 변환
+  
   res.send(fileNames)
 })
 
@@ -71,20 +63,6 @@ app.listen(port, () => {
 });
 
 // 요청 파일 라우팅
-// Static File(정적 파일)
-// 정적 파일이란, 직접 값에 변화를 주지 않는 이상 변하지 않는 파일을 의미합니다. 
-// 예를 들면, image, css 파일, js 파일 등을 의미합니다. 
-// express는 이러한 정적 파일들을 손쉽게 제공할 수 있는 기능을 가지고 있습니다.
-// express 변수에는 stastic이라는 메서드가 포함되어있습니다. 
-// 이 메서드를 미들웨어로서 로드해줍니다. 
-// static의 인자로 전달되는 'public'은 디렉터리의 이름입니다. 
-// 따라서 'public' 이라는 디렉터리 밑에 있는 데이터들은 웹브라우저의 요청에 따라 서비스를 제공해줄 수 있습니다.
-// 가령, 사용자가 127.0.0.1:3000/images/cat.jpg 로 접근한다면, 
-// 해당 파일을 public/images/cat.jpg에 존재하는지 검색하게 됩니다
-// https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=pjok1122&logNo=221545195520
-
-//서버는 띄어 놓고, 하위 주소로 관련 문서들이나 프로젝트를 접근하는 개념이다.
-//index.html 파일 이름은 바꾸면 안됩니다. 웹사이트에 접속했을때 정적 폴더 세팅이 되어 있다면 그 폴더 안에 index파일이 있는지를 가장 먼저 찾도록 되어 있습니다.
 // __dirname 은 node 에서 제공하는 node 파일의 경로를 담고 있는 변수
 app.use(express.static(path.join(__dirname, 'public')));  //root 개념으로 설정??
 app.use(express.static('uploads'));
